97fe6ab0e94ef1767403ee6281b569753083d62a
Brandon Tor <brandon.tor@hotmail.com>
2023-05-12 14:18:13 -0400

    welcome flow is working for various flags

--- index.js
+++ index.js
@@ -10,17 +10,27 @@ require('dotenv').config()
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
-const notion = require('./utils/notion');
+const notion  = require('./utils/notion');
+
 
 const input = cli.input;
 const flags = cli.flags;
-const { clear, debug } = flags;
+const { clear, debug, add, get, update } = flags;
 
 (async () => {
 	init({ clear });
+
 	input.includes(`help`) && cli.showHelp(0);
 
-	flags.add && (await notion())
+	//if there is a flag then jump straight to the corresponding flag path
+	add && (await notion("Add"))
+	get && (await notion("Get"))
+	update && (await notion("Update"))
+	
+	//Otherwise start from the welcome prompt
+	if (!add && !get && !update) {
+		await notion()
+	}
 
 	debug && log(flags);
 })();
